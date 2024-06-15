f68e8e43493a9b6760b29d438a77bc07513f8adb
ChrysanthosM <ChrysanthosM@Outlook.com>
2024-06-15 18:31:34 +0300

    fixes

--- src/main/Java/cf5/utils/Converters.java
+++ src/main/Java/cf5/utils/Converters.java
@@ -2,10 +2,6 @@ package cf5.utils;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public final class Converters {
     public static String getFullName(String firstName, String lastName) {
         return StringUtils.trimToEmpty(firstName).concat(StringUtils.SPACE).concat(StringUtils.trimToEmpty(lastName)).trim();
