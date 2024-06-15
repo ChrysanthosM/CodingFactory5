852df22edf52983805dd0fc0b1dd59f632fb2287
ChrysanthosM <ChrysanthosM@Outlook.com>
2024-06-15 18:31:13 +0300

    fixes

--- src/main/Java/cf5/utils/Converters.java
+++ src/main/Java/cf5/utils/Converters.java
@@ -7,21 +7,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public final class Converters {
-    public static Date convertStringToDate(String dateString) throws ParseException {
-        if (StringUtils.isBlank(dateString)) return null;
-
-        SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy");
-        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyyMMdd");
-
-        Date date = inputFormat.parse(dateString);
-        String formattedDateString = outputFormat.format(date);
-        return outputFormat.parse(formattedDateString);
-    }
-    public static String convertDateToString(Date date) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        return formatter.format(date);
-    }
-
     public static String getFullName(String firstName, String lastName) {
         return StringUtils.trimToEmpty(firstName).concat(StringUtils.SPACE).concat(StringUtils.trimToEmpty(lastName)).trim();
     }
